3b9d6689e9fc1b99b3761147d203ac614a414409
pulsar541 <yesu@mail.ru>
2021-06-09 21:27:35 +0300

    a

---
+++ Assets/scripts/Global.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global  
+{
+    
+    public static int CUBES_I = 100; 
+    public static int CUBES_J = 100;
+    public static int CUBES_K = 100;
+    
+    public static int[,,] cubes;
+}
