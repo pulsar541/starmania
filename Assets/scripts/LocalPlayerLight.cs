bd79de6b3a03882b893957b4ab5660852f6a7b4a
pulsar541 <yesu@mail.ru>
2021-09-16 15:38:45 +0300

    Intensity of player light depents on level

---
+++ Assets/scripts/LocalPlayerLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerLight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Light light;
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = transform.position.y / 100.0f; 
+    }
+}
